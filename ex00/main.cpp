4bde9a069a2c5cf6286eecb1517ddb53273790f7
Suhyeon AHN <suahn@e2r9p3.clusters.42paris.fr>
2023-03-13 20:15:53 +0100

    date checker added

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,6 +4,32 @@
 #include<algorithm>
 #include <set>
 
+const int MAX_YR = 9999;
+const int MIN_YR = 1800;
+  
+bool isLeap(int y)
+{
+    return (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0));
+}
+  
+bool isValidDate(int d, int m, int y)
+{
+    if (y > MAX_YR || y < MIN_YR || m < 1 || m > 12 || d < 1 || d > 31)
+        return false;
+  
+    if (m == 2)
+    {
+        if (isLeap(y))
+            return (d <= 29);
+        else
+            return (d <= 28);
+    }
+    if (m == 4 || m == 6 || m == 9 || m == 11)
+        return (d <= 30);
+
+    return true;
+}
+
 int main() {
     std::fstream newFile;
 
@@ -31,6 +57,6 @@ int main() {
         for (std::set<std::pair <std::string,std::string> >::iterator it=set.begin(); it!=set.end(); ++it) {
             std::cout << "?" << it->first << "/"  << it->second << "?"<< std::endl;
         }
-        
+
     }
 }
